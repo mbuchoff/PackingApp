4ab80f19be53f2287303c4c264a1939deb18941e
Michael Buchoff <mbuchoff@gmail.com>
2018-05-06 13:53:39 -0400

    Somehow not all the files got committed in the last commit.  Trying again.

--- app/src/main/java/buchoff/michael/packingapp/MainActivity.java
+++ app/src/main/java/buchoff/michael/packingapp/MainActivity.java
@@ -1,15 +1,20 @@
 package buchoff.michael.packingapp;
 
+import android.app.ActionBar;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout _linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        _linearLayout = (LinearLayout) findViewById(R.id.LinearLayout);
     }
 
     public void plusButtonClicked(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+        TextInputEditText newTextInput = new TextInputEditText(this);
+        newTextInput.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
+        newTextInput.setBackgroundResource(R.drawable.border);
+        _linearLayout.addView(newTextInput);
     }
 
     @Override
