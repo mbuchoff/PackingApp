1396f72d08cd642614f0ccdee5e61fc0b2455f96
Michael Buchoff <mbuchoff@gmail.com>
2018-05-11 13:07:10 -0400

    Added a listview that adds text boxes

--- app/src/main/java/buchoff/michael/packingapp/MainActivity.java
+++ app/src/main/java/buchoff/michael/packingapp/MainActivity.java
@@ -1,36 +1,41 @@
 package buchoff.michael.packingapp;
 
-import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout _linearLayout;
+    ListView _listView;
+    LayoutInflater _layoutInflater;
+    ArrayAdapter<String> _adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        _linearLayout = (LinearLayout) findViewById(R.id.LinearLayout);
+        _linearLayout = findViewById(R.id.LinearLayout);
+        _listView = findViewById(R.id.ListView);
+        _layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        _adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1);
+        _listView.setAdapter(_adapter);
     }
 
     public void plusButtonClicked(View view) {
-        TextInputEditText newTextInput = new TextInputEditText(this);
-        newTextInput.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
-        newTextInput.setBackgroundResource(R.drawable.border);
-        _linearLayout.addView(newTextInput);
+        _adapter.add("ASDF");
     }
 
     @Override
