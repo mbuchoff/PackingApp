8d45e2316d8a5842637cc0dc4eb82363cacd48c2
Michael Buchoff <mbuchoff@gmail.com>
2018-05-16 13:28:14 -0400

    Used observablefields with the GUI.

--- app/src/main/java/buchoff/michael/packingapp/TodoItem.java
+++ app/src/main/java/buchoff/michael/packingapp/TodoItem.java
@@ -1,13 +1,19 @@
 package buchoff.michael.packingapp;
 
 import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
 import android.os.Bundle;
+import android.view.View;
 
 public class TodoItem extends ViewModel {
     private static int i = 0;
 
-    public TodoItem() { firstName = new Integer(i++).toString();}
+    public TodoItem() { firstName.set(Integer.valueOf(i++).toString());}
 
-    public String firstName;
+    public ObservableField<String> firstName = new ObservableField<>();
     public final String name = "TheName";
+
+    public void onClicksAreHard(View view) {
+        firstName.set(firstName.get() + "A");
+    }
 }
